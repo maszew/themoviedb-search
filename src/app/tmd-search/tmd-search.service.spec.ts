aa83fc5b8129bd9f1a20de2093218258109edf99
Marcel Szewczyk <marcel.szewczyk@gmail.com>
2017-09-06 21:44:25 +0200

    tmd-search.service implemented with unit tests

--- src/app/tmd-search/tmd-search.service.spec.ts
+++ src/app/tmd-search/tmd-search.service.spec.ts
@@ -9,12 +9,16 @@ import {
 import { MockBackend } from '@angular/http/testing';
 
 import { TmdSearchService } from './tmd-search.service';
+import { SearchQueryParameters } from './search-query-parameters.model'
 
 describe('TmdSearchService', () => {
-
   const mockResponse = {
     id: '5'
   };
+
+  function customEquality(expected: any, value: any): boolean {
+    return JSON.stringify(expected).trim() === JSON.stringify(value).trim();
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -24,17 +28,61 @@ describe('TmdSearchService', () => {
         {provide: XHRBackend, useClass: MockBackend}
       ]
     });
+    jasmine.addCustomEqualityTester(customEquality);
   });
 
   it('should be created',
-    inject([TmdSearchService, XHRBackend], (service, mockBackend) =>
+    inject([TmdSearchService], (service) =>
   {
-    mockBackend.connections.subscribe((connection) => {
-      connection.mockRespond(new Response(new ResponseOptions({
-        body: JSON.stringify(mockResponse)
-      })));
-    });
-
     expect(service).toBeTruthy();
   }));
+
+  it('should return when no query provided',
+    inject([TmdSearchService, Http], (service, http) =>
+  {
+    expect(service).toBeTruthy();
+    let queryParameters = new SearchQueryParameters();
+    spyOn(http, 'get').and.callThrough();
+    expect(service.searchMovie('/search/movies', queryParameters)).toBeNull();
+    expect(http.get).not.toHaveBeenCalled();
+  }));
+
+  it('should generate proper GET request - query provided', function(done) {
+    inject([TmdSearchService, XHRBackend, Http], (service, mockBackend, http) => {
+      mockBackend.connections.subscribe((connection) => {
+        connection.mockRespond(new Response(new ResponseOptions({
+          body: JSON.stringify(mockResponse)
+        })));
+      });
+      expect(service).toBeTruthy();
+      let queryParameters = new SearchQueryParameters();
+      queryParameters.query = 'Matrix';
+      spyOn(http, 'get').and.callThrough();
+      service.searchMovie('/search/movies', queryParameters).subscribe(res => {
+        expect(res).toEqual({id:'5'});
+        done();
+      });
+      expect(http.get).toHaveBeenCalled();
+    })();
+  });
+
+  it('should generate proper GET request with custom parameters', function(done) {
+    inject([TmdSearchService, XHRBackend, Http], (service, mockBackend, http) => {
+      mockBackend.connections.subscribe((connection) => {
+        connection.mockRespond(new Response(new ResponseOptions({
+          body: JSON.stringify(mockResponse)
+        })));
+      });
+      expect(service).toBeTruthy();
+      let queryParameters = new SearchQueryParameters();
+      queryParameters.query = 'Pirates';
+      queryParameters.include_adult = true;
+      spyOn(http, 'get').and.callThrough();
+      service.searchMovie('/search/movies', queryParameters).subscribe(res => {
+        expect(res).toEqual({id:'5'});
+        done();
+      });
+      expect(http.get).toHaveBeenCalled();
+    })();
+  });
 });
--- src/app/tmd-search/tmd-search.service.ts
+++ src/app/tmd-search/tmd-search.service.ts
@@ -1,11 +1,44 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, URLSearchParams, QueryEncoder } from '@angular/http';
+import 'rxjs/add/operator/map';
 
-import * as config from "./config.json";
+import * as config from './config.json';
+import { SearchQueryParameters } from './search-query-parameters.model'
 
 @Injectable()
+/**
+ * Service to communicate with 'The Movie Database' API
+ * @class
+ */
 export class TmdSearchService {
 
   constructor(private http: Http) {}
 
+  /**
+   * Main service's method to search for a movie
+   * @param {string} route - route which is used to get JSON data from the API
+   * @param {SearchQueryParameters} queryParameters - query parameters for the GET request
+   *
+   * @return {Observable} - returns Observable to be handled outside
+   */
+  searchMovie(route: string, queryParameters: SearchQueryParameters) {
+      if(queryParameters.query === '') {
+        return null;
+      }
+      let endpoint: string = config['api-url'] + route;
+      let queryParametersKeys: Array<string> = Object.keys(queryParameters);
+      let queryEncoder: QueryEncoder = new QueryEncoder();
+      let searchParameters: URLSearchParams = new URLSearchParams();
+
+      searchParameters.set('api_key', 'af1268643901e99d9bd71f7e55b1fa21');//config['api-key']);
+
+      for(let key of queryParametersKeys) {
+        if(typeof queryParameters[key] !== 'undefined') {
+          searchParameters.set(key, queryEncoder.encodeKey(queryParameters[key]));
+        }
+      }
+
+      return this.http.get(endpoint, {search: searchParameters})
+        .map(res => res.json());
+  }
 }
